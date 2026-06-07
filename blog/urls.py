6fb5997627c152e029c13ab8ab626caf7424b6cd
Oleksandr Dvirko <dvalex02@gmail.com>
2026-06-07 23:31:28 +0300

    solution

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from blog.views import IndexView, post_detail
 app_name = "blog"
 
 urlpatterns = [
-    path("post/", IndexView.as_view(), name="index"),
-    path("post/<int:pk>", post_detail, name="post-detail"),
+    path("", IndexView.as_view(), name="index"),
+    path("posts/<int:pk>/", post_detail, name="post-detail"),
 ]
