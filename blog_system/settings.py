3b9cd25608046506c90d560437793dcd92980b61
Oleksandr Dvirko <dvalex02@gmail.com>
2026-06-07 21:12:51 +0300

    developing

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'blog_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'blog.User'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
