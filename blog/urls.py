20f1af9571076b4330919686a2ab7148c9191f3f
Oleksandr Dvirko <dvalex02@gmail.com>
2026-06-07 23:13:02 +0300

    debuging

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from blog.views import index
-
+from blog.views import IndexView, post_detail
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('post/', IndexView.as_view(), name='index'),
+    path('post/<int:pk>', post_detail, name='post-detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,49 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView
+
+from blog.forms import CommentaryForm
+from blog.models import Post, Commentary
 
 
-def index(request) -> HttpResponse:
-    return render(request,'blog/index.html')
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    queryset = Post.objects.order_by('-created_time')
+    paginate_by = 5
+    context_object_name = 'posts'
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    comments = Commentary.objects.filter(post=post).select_related('user').order_by('-created_time')
+
+    if request.method == 'POST':
+        form = CommentaryForm(request.POST)
+
+        if not request.user.is_authenticated:
+            form.is_valid()
+            form.add_error(
+                None,
+                'Only authorized users can post comments.',
+            )
+        elif form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.user = request.user
+            comment.save()
+
+            return redirect(
+                'blog:post-detail',
+                pk=post.pk,
+            )
+    else:
+        form = CommentaryForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    }
+
+    return render (request, 'blog/post-detail.html', context=context)
